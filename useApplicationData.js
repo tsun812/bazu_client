b5debd109e027442d2b2befbe7e9d5d83b2093e7
Yan Sun <tsun812@gmail.com>
2022-04-28 15:13:32 -0700

    create CRUD operation with aaxios api on client side

--- useApplicationData.js
+++ useApplicationData.js
@@ -4,6 +4,36 @@ import axios from "axios";
 export default function useApplicationData() {
   const [id, setId] = useState("");
   const [mode, setMode] = useState("user");
-  const [promo, setPromo] = useState("");
-  return { id, mode, promo, setId, setMode, setPromo };
+  const [promo, setPromo] = useState(null);
+
+  const fetchAPI = () => {
+    Promise.all([axios.get(`/users/{id}`)]).then((all) => {
+      setMode(all[0]["data"].promo);
+    });
+  };
+
+  const updatePromo = (id, newPromo) => {
+    let params = newPromo;
+    axios.put(`/users/${id}`, params).then((all) => {
+      setPromo(newPromo);
+    });
+  };
+
+  const deletePromo = (id) => {
+    axios.put(`/users/${id}`, null).then((all) => {
+      setPromo(null);
+    });
+  };
+
+  return {
+    id,
+    mode,
+    promo,
+    setId,
+    setMode,
+    setPromo,
+    fetchAPI,
+    updatePromo,
+    deletePromo,
+  };
 }
